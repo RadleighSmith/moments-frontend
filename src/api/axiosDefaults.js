9d2a49fc5c63b384f33fec83cf7e0d92038c7dca
Radleigh Smith <RadleighMSmith@gmail.com>
2024-05-04 16:49:49 +0000

    add tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://moments-drf-api-rs-34ce7500817a.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
