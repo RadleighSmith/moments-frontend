4eb508e0f83d0484df0f9c14458233e29e672a5f
Radleigh Smith <RadleighMSmith@gmail.com>
2024-05-04 14:31:49 +0000

    Add sign in and sign up pages

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://moments-drf-api-rs-34ce7500817a.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
